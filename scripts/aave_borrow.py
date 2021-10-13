fdf5e839b559d425e0167983e315e3b304a91cc2
rcolejnr <ricardo.m.jnr@gmail.com>
2021-10-13 23:44:32 +0300

    first commit

---
+++ scripts/aave_borrow.py
@@ -0,0 +1,122 @@
+from typing import NewType
+
+from eth_account import account
+from scripts.get_weth import get_weth
+from scripts.helpful_scripts import get_account
+from brownie import config, network, interface
+from web3 import Web3
+
+# 0.1
+amount = Web3.toWei(0.1, "ether")
+
+
+def main():
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth()
+
+    # abi, address
+    lending_pool = get_lending_pool()
+    # applove sendiing out ERC20 tokens
+    approve_erc20(amount, lending_pool.address, erc20_address, account)
+    print("Depositing...")
+    tx = lending_pool.deposit(
+        erc20_address, amount, account.address, 0, {"from": account}
+    )
+    tx.wait(1)
+    print("Depositted")
+    # how much can we borrow
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+
+    # Borrow DAI
+    dai_eth_price = get_asset_price(
+        config["networks"][network.show_active()]["dai_eth_price_feed"]
+    )
+    amount_dai_to_borrow = (1 / dai_eth_price) * (borrowable_eth * 0.95)
+    # convert borrowable eth to borrowable dai * 95%
+    print("We are going to borrow {amount_dai_to_borrow}")
+
+    dai_address = config["networks"][network.show_active()]["dai_token"]
+    borrow_tx = lending_pool.borrow(
+        dai_address,
+        Web3.toWei(amount_dai_to_borrow, "ether"),
+        1,
+        0,
+        account.address,
+        {"from": account},
+    )
+    borrow_tx.wait(1)
+    print("We borrowed some dai")
+    get_borrowable_data(lending_pool, account)
+    print("You are repaying all of ur dai")
+    repay_all(amount, lending_pool, account)
+    print("You just deposited, borrowed, and repayed with Aave, Brownie, and chianlink")
+    print("")
+    get_borrowable_data(lending_pool, account)
+
+
+def repay_all(amount, lending_pool, account):
+    approve_erc20(
+        Web3.toWei(amount, "ether"),
+        lending_pool,
+        config["networks"][network.show_active()]["dai_token"],
+        account,
+    )
+    repay_tx = lending_pool.repay(
+        config["networks"][network.show_active()]["dai_token"],
+        amount,
+        1,
+        account.address,
+        {"from": account},
+    )
+    repay_tx.wait(1)
+
+
+def get_asset_price(price_feed_address):
+    dai_eth_price_feed = interface.IAggregatorV3(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    converted = Web3.fromWei(latest_price, "ether")
+    print("Dai eth price is {converted}")
+    return float(converted)
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited")
+    print(f"You have {total_debt_eth} worth of ETH borrowed")
+    print(f"You can borrow {available_borrow_eth} worth of ETH deposited")
+    return (float(available_borrow_eth), float(total_debt_eth))
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 token..")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved")
+    return tx
+
+
+def get_lending_pool():
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
+
+
+"""
+mainnet-fork is literally grabbing a fake mainnet, and do your testing in it
+"""
